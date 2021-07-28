ceb4abf0c983ebbb93b149aae8a1f985db31d150
cmj <chmin82@gmail.com>
2021-07-28 23:47:21 +0900

    test commit 3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = precess.env.PORT;
+const PORT = precess.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send({ hello: "world" });
