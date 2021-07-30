f50c0727ee2769f4f6493690db9c2319ff08e21c
cmj <chmin82@gmail.com>
2021-07-30 23:31:28 +0900

    update db

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -1,8 +1,8 @@
 
 var mysql = require('mysql');
 var db = mysql.createConnection({
-  host : '192.168.0.2',
-  port : '3307',
+  host : 'ymjm.iptime.org',
+  port : '5307',
   user : 'heroku-user',
   password : 'Heroku1234!',
   database : 'heroku-node-api'
