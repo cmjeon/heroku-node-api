7ceea98d19a89df357e41d4dc2564840b2e6bad0
cmj <chmin82@gmail.com>
2021-08-05 00:07:53 +0900

    add api docs

---
+++ utils/api-doc.js
@@ -0,0 +1,13 @@
+// import swaggerUi from 'swagger-ui-express';
+var swaggerUi = require('swagger-ui-express');
+// import swaggerDocument from './api-spec.json';
+var swaggerDocument = require('./api-spec.json')
+// import { Router } from 'express';
+// const router = Router();
+const express = require('express');
+const router = express.Router();
+
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.use('/api/v1', router);
+
+module.exports = router;
