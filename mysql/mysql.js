4398607fd390ecea1fa924d1b1c26f37d53148aa
cmj <chmin82@gmail.com>
2021-08-14 10:20:40 +0900

    separate mysql config config.json

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -1,11 +1,6 @@
 
 var mysql = require('mysql');
-var db = mysql.createConnection({
-  host : 'ymjm.iptime.org',
-  port : '5307',
-  user : 'heroku-user',
-  password : 'Heroku1234!',
-  database : 'heroku-node-api'
-});
+var config = require('./config.json')
+var db = mysql.createConnection(config);
 db.connect();
 module.exports = db;
