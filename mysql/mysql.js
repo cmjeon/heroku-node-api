ed91b03727a6bdb520844392a42e4e348948b651
cmj <chmin82@gmail.com>
2021-08-15 19:05:45 +0900

    update mysql.js

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -1,5 +1,5 @@
 var mysql = require('mysql');
 var config = require('./config.json')
-var db = mysql.createConnection({ config });
+var db = mysql.createConnection(config);
 db.connect();
 module.exports = db;
