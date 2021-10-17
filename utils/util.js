e1b2d74c842bdc6ecc35872d4d9f8c32b1eeb76c
cmj <chmin82@gmail.com>
2021-10-17 22:34:00 +0900

    remove getRandomEmail from util.js

--- utils/util.js
+++ utils/util.js
@@ -1,14 +1,5 @@
 const getRandomID = (length) => [...Array(length)].map(() => (~~(Math.random() * 36)).toString(36)).join('').toUpperCase();
 
-const getRandomEmail = () => {
-  var chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
-  var emailString = '';
-  for (var i = 0; i < 10; i++) {
-    emailString += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return emailString + '@test.com';
-}
-
 const getTodayDateWithHypen = () => {
   let today = new Date();
   let todayDateString = today.getUTCFullYear() + "-" + ("0" + (today.getUTCMonth() + 1)).slice(-2) + "-" + ("0" + today.getUTCDate()).slice(-2);
@@ -29,7 +20,6 @@ const getNowTime = () => {
 
 module.exports = {
   getRandomID,
-  getRandomEmail,
   getTodayDateWithHypen,
   getNowTime
 }
