f65c395ac5795e1a08aa264fb021fdc39be98767
cmj <chmin82@gmail.com>
2021-10-18 20:04:50 +0900

    Expiration_date

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const SECRET_KEY = 'heroku-node-api';
-const EXPIRATION_DATE = '1d';
+const EXPIRATION_DATE = '7d'; // 60s, 7d
 const APIID_OPENWEATHER = '363b9090b9fee032857eb62d43b83921';
 
 module.exports = {
