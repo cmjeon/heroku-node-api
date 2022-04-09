1bd70ad27b418d7c11de045c53367d6c839708ac
cmj <chmin82@gmail.com>
2022-04-09 10:04:25 +0900

    let 으로 전환

--- src/mysql/mysql.js
+++ src/mysql/mysql.js
@@ -1,7 +1,7 @@
-var config = require('../config/config.json')
-var mysql1 = require('mysql');
-var mysql2 = require('mysql2');
-var db = mysql1.createConnection(config);
+const config = require('../config/config.json');
+const mysql1 = require('mysql');
+const mysql2 = require('mysql2');
+const db = mysql1.createConnection(config);
 const db2 = mysql2.createPool(config);
 db.connect();
 const db2Promise = db2.promise();
