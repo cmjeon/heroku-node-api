6070342335bdcaa144f145f4722df8ca30cd3013
cmj <chmin82@gmail.com>
2022-04-16 11:38:42 +0900

    db 커넥션 실패항 경우에도 서버 유지되도록 변경

--- src/mysql/mysql.js
+++ src/mysql/mysql.js
@@ -1,12 +1,23 @@
 const config = require('../config/config.json');
 const mysql1 = require('mysql');
 const mysql2 = require('mysql2');
-const db = mysql1.createConnection(config);
-const db2 = mysql2.createPool(config);
-db.connect();
-const db2Promise = db2.promise();
+let mysqlConnection, db2Promise;
+
+mysqlConnection = mysql1.createConnection(config);
+mysqlConnection.connect(function(err) {              // The server is either down
+  if(err) {                                     // or restarting (takes a while sometimes).
+    console.log('DB Connection ERROR1:', err);
+  }
+});
+const mysql2pool = mysql2.createPool(config);
+db2Promise = mysql2pool.promise(function(err) {
+  if(err) {                                     // or restarting (takes a while sometimes).
+    console.log('DB Connection ERROR2:', err);
+  }
+});
+
 
 module.exports = {
-  db,
+  db: mysqlConnection,
   db2Promise
 };
