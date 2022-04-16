08d5350a31ad10594664e8db4193ec6ccf60a3e2
cmj <chmin82@gmail.com>
2022-04-16 11:50:24 +0900

    add config/index.js

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+const SECRET_KEY = 'heroku-node-api';
+const EXPIRATION_DATE = '7d'; // 60s, 7d
+const APIID_OPENWEATHER = '363b9090b9fee032857eb62d43b83921';
+
+module.exports = {
+  SECRET_KEY,
+  EXPIRATION_DATE,
+  APIID_OPENWEATHER
+}
