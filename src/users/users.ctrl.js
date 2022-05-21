9abbb7d2f13fd196ae376dcd164a7ee4e6deffc6
cmj <chmin82@gmail.com>
2022-05-21 11:37:14 +0900

    [feat] db -> db2Promise 로 변경
    - async/await 사용

--- src/users/users.ctrl.js
+++ src/users/users.ctrl.js
@@ -1,4 +1,4 @@
-var { db } = require('../mysql/mysql');
+var { db2Promise } = require('../mysql/mysql');
 
 const list = (req, res) => {
   req.query.limit = req.query.limit || 10;
@@ -6,14 +6,14 @@ const list = (req, res) => {
   if (Number.isNaN(limit)) {
     return res.status(400).end();
   }
-  db.query(`SELECT USER_ID, EMAIL, NAME, PROFILE FROM USER_BASE_INFO limit ${limit}`, (err, users) => {
-    if (err) {
-      console.log('err');
-      throw err;
-    }
-    res.json(users);
-    res.status(200).end();
-  });
+  const [rows1, defs1, err1] = db2Promise.query(`SELECT USER_ID, EMAIL, NAME, PROFILE FROM USER_BASE_INFO limit ${limit}`);
+  if (err1) {
+    console.log('err1');
+    throw err1;
+  }
+  const users = rows1;
+  res.json(users);
+  res.status(200).end();
 }
 
 const show = (req, res) => {
@@ -25,20 +25,26 @@ const show = (req, res) => {
   //   return res.status(400).end();
   // }
   // console.log('id', id);
-  db.query(`SELECT USER_ID, EMAIL, NAME, PROFILE FROM USER_BASE_INFO WHERE USER_ID = '${id}'`, (err, users) => {
-    // console.log('users', users);
-    const user = users[0];
-    if (!user) return res.status(404).end();
-    res.json(user);
-  });
+  const [rows1, defs1, err1] = db2Promise.query(`SELECT USER_ID, EMAIL, NAME, PROFILE FROM USER_BASE_INFO WHERE USER_ID = '${id}'`);
+  // console.log('users', users);
+  if (err1) {
+    console.log('err1');
+    throw err1;
+  }
+  const user = rows1[0];
+  if (!user) return res.status(404).end();
+  res.json(user);
 }
 
 const destroy = (req, res) => {
   const id = req.params.id;
-  db.query(`DELETE FROM USER_BASE_INFO WHERE USER_ID = '${id}'`, (err, users) => {
-    // console.log('users', users);
-    res.status(204).end();
-  });
+  const [rows1, defs1, err1] = db2Promise.query(`DELETE FROM USER_BASE_INFO WHERE USER_ID = '${id}'`)
+  // console.log('users', users);
+  if (err1) {
+    console.log('err1');
+    throw err1;
+  }
+  res.status(204).end();
 }
 
 module.exports = {
