d746675f20f76bb46efdcc1d6c58ae660bb2fcc1
cmj <chmin82@gmail.com>
2022-05-22 18:38:52 +0900

    [feat] pg 설정파일
    - 외부 환경변수로 받도록 변경

---
+++ src/postgresql/postgresql.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const config = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+}
+const pool = new Pool(config)
+pool.connect(err => {
+  if (err) {
+    console.log('Failed to connect db ' + err)
+  } else {
+    console.log('Connect to db done!')
+  }
+})
+
+module.exports = {
+  pool: pool
+};
