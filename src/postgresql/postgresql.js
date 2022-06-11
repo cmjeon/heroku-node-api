a386217d91ec2c942a6b68fb3278e8e804fc7afa
cmj <chmin82@gmail.com>
2022-06-11 10:25:52 +0900

    [feat] Postgresql 설정 변경
    - ssl 설정 변경

--- src/postgresql/postgresql.js
+++ src/postgresql/postgresql.js
@@ -6,18 +6,8 @@ const config = {
   password: process.env.DB_PW,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false
-  }
 }
 const pool = new Pool(config)
-// pool.connect(err => {
-//   if (err) {
-//     console.log('Failed to connect db ' + err)
-//   } else {
-//     console.log('Connect to db done!')
-//   }
-// })
 
 module.exports = {
   pool
