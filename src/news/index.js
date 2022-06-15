e97b1e27c4b09f6edc37447fc35e733a153e6239
cmj <chmin82@gmail.com>
2022-06-15 20:57:20 +0900

    [feat] news
    - 네이버 뉴스검색 추가

--- src/news/index.js
+++ src/news/index.js
@@ -5,5 +5,6 @@ const ctrl = require('./news.ctrl');
 router.get('/', ctrl.index);
 router.get('/yh/rss/newest', ctrl.yhRssNewest)
 router.get('/yh/rss/headline', ctrl.yhRssHeadline);
+router.get('/naver/search', ctrl.naverSearch);
 
 module.exports = router;
