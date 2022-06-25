14e0fb90b43854a991b68bc5fc9a7b41522334e3
cmj <chmin82@gmail.com>
2022-06-25 10:56:16 +0900

    [FEAT] TRY/CATCH
    - auth
    - tasks

--- src/auth/auth.ctrl.js
+++ src/auth/auth.ctrl.js
@@ -35,14 +35,14 @@ const login = async (req, res) => {
       message: 'Login Success',
       token: token,
     });
-  } catch (err) {
-    console.log(err)
+  } catch (e) {
+    console.log(e)
     return res.status(500).send('Internal Server Error');
   }
 }
 
 const logout = (req, res) => {
-
+  // TODO 구현
 }
 
 const signup = async (req, res) => {
@@ -64,29 +64,34 @@ const signup = async (req, res) => {
       user: newUser,
       message: 'Signup Success'
     });
-  } catch(err) {
-    console.log(err);
+  } catch(e) {
+    console.log(e);
     return res.status(500).json('Internal Server Error');
   }
 }
 
 const checkDuplEmail = async (req, res) => {
-  const email = req.body.email;
-  const existUser = await getUserInfo({ email })
-  if(existUser) {
-    console.log('### Exist Email')
-    return res.status(409).json({
-      success: false,
-      user: { email: email },
-      message: 'Exist Email'
-    }).end();
+  try {
+    const email = req.body.email;
+    const existUser = await getUserInfo({email})
+    if (existUser) {
+      console.log('### Exist Email')
+      return res.status(409).json({
+        success: false,
+        user: {email: email},
+        message: 'Exist Email'
+      }).end();
+    }
+    const result = {
+      success: true,
+      user: {email: email},
+      message: 'NotExist Email'
+    }
+    return res.status(200).json(result);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json('Internal Server Error');
   }
-  const result = {
-    success : true,
-    user: { email: email },
-    message: 'NotExist Email'
-  }
-  return res.status(200).json(result);
 }
 
 const hashPassword = async (pw) => {
@@ -105,21 +110,24 @@ const getUserInfo = async (condition) => {
 
     let { rows } = await pool.query(`SELECT USER_ID, PW, EMAIL, NAME, PROFILE FROM USER_BASE_INFO WHERE ${ where }`);
     return rows[0]
-  } catch(err) {
-    console.log(err);
-    throw err;
+  } catch(e) {
+    throw e;
   }
 }
 
 const createUserInfo = async (body) => {
-  const id = getRandomID(16);
-  const hashedPw = await hashPassword(body.pw);
-  const statement = {
-    text: 'INSERT INTO USER_BASE_INFO (USER_ID, EMAIL, NAME, PW, PROFILE, CRET_DTIME, CRET_ID, MOD_DTIME, MOD_ID) VALUES ( $1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING USER_ID',
-    values: [id, body.email, body.name, hashedPw, body.profile, new Date(), id, new Date(), id]
+  try {
+    const id = getRandomID(16);
+    const hashedPw = await hashPassword(body.pw);
+    const statement = {
+      text: 'INSERT INTO USER_BASE_INFO (USER_ID, EMAIL, NAME, PW, PROFILE, CRET_DTIME, CRET_ID, MOD_DTIME, MOD_ID) VALUES ( $1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING USER_ID',
+      values: [id, body.email, body.name, hashedPw, body.profile, new Date(), id, new Date(), id]
+    }
+    const {rows} = await pool.query(statement);
+    return rows[0].user_id;
+  } catch (e) {
+    throw e;
   }
-  const { rows } = await pool.query(statement);
-  return rows[0].user_id;
 }
 
 module.exports = {
