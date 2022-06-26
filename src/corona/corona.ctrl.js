b48ac3e7932eec967f025aa08a0df80468c36ca6
cmj <chmin82@gmail.com>
2022-06-26 18:56:21 +0900

    [CHORE] Corona
    - corona 용 instance
    - index 에 corona 모듈 추가
    - controller 추가

---
+++ src/corona/corona.ctrl.js
@@ -0,0 +1,43 @@
+const { coronaInstance } = require('../utils/api.js');
+const { CORONA_SVC_KEY } = process.env
+
+const index = (req, res) => {
+  res.json('Corona!');
+}
+
+const koreaBeta = async (req, res) => {
+  try {
+    const serviceKey = CORONA_SVC_KEY
+    const url =  `korea/beta/?serviceKey=${serviceKey}`
+
+    cons가t result = await coronaInstance({
+      method : 'get',
+      url : url
+    })
+
+    res.status(200).json(result.data).end();
+  } catch(e) {
+    console.log(e)
+    return res.status(500).send('Internal Server Error');
+  }
+}
+
+const koreaVaccine = async (req, res) => {
+  res.json('Corona!');
+}
+
+const koreaCounter = async (req, res) => {
+  res.json('Corona!');
+}
+
+const koreaCountry = async (req, res) => {
+  res.json('Corona!');
+}
+
+module.exports = {
+  index,
+  koreaBeta,
+  koreaVaccine,
+  koreaCounter,
+  koreaCountry
+}
