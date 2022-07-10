ac28a744af515a12eb277bf854fac3ae0245c23d
cmj <chmin82@gmail.com>
2022-07-10 13:44:49 +0900

    [CHORE] news-keyword
    - 미사용 news URL 삭제

--- src/news/index.js
+++ src/news/index.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const ctrl = require('./news.ctrl');
 
 router.get('/', ctrl.index);
-router.get('/yh/rss/newest', ctrl.yhRssNewest)
-router.get('/yh/rss/headline', ctrl.yhRssHeadline);
+// 미사용
+// router.get('/yh/rss/newest', ctrl.yhRssNewest)
+// 미사용
+// router.get('/yh/rss/headline', ctrl.yhRssHeadline);
 router.get('/naver/search', ctrl.naverSearch);
 router.get('/naver/news-keywords', ctrl.naverNewsKeywords);
-router.get('/naver/crawl', ctrl.naverCrawl);
+// 미사용
+// router.get('/naver/crawl', ctrl.naverCrawl);
 
 module.exports = router;
